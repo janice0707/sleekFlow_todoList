6b2dbf4571cf626ff6a1ba54efc4237a8a9c450e
janice0707 <kangcy0721@hotmail.com>
2024-02-29 01:06:42 +0800

    SQL create table script

---
+++ Todo.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE [SleekFlow];
+GO
+
+
+USE [SleekFlow]
+GO
+
+/****** Object:  Table [dbo].[Todo]    Script Date: 2/29/2024 12:47:19 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Todo](
+	[UniqueID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](100) NOT NULL,
+	[Description] [nvarchar](500) NULL,
+	[DueDate] [datetime] NULL,
+	[Status] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_Todo] PRIMARY KEY CLUSTERED 
+(
+	[UniqueID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
